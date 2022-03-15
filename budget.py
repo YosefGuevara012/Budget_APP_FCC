8b0a09e58f8853a441e357c2082c6a69a866ddc0
YOSEF GUEVARA <yosef.guevara012@hotmail.com>
2022-03-15 18:03:24 -0500

    Fisrt methods working

--- budget.py
+++ budget.py
@@ -1,5 +1,27 @@
 class Category:
 
+    def __init__(self, name):
+        self.name = name
+        self.ledger = []
+        self.budget = 0
+
+    def deposit(self, amount, description = ""):
+        self.amount = amount
+        self.ledger.append({"amount": self.amount, "description": description})
+        self.budget += amount
+
+    def withdraw(self, amount):
+      if self.amount >= amount:
+        self.ledger.append(amount * -1)
+        self.budget -= amount
+        return True
+      else:
+        return False
+
+    def get_balance(self):
+        return self.budget
+
+      
         
 
         
