56de5bb9684d5f0012e5e045da6b447369631659
YOSEF GUEVARA <yosef.guevara012@hotmail.com>
2022-03-17 12:16:08 -0500

    Project done all test OK

--- budget.py
+++ budget.py
@@ -57,31 +57,36 @@ class Category:
       
 def create_spend_chart(categories):
   
-
+    # Calculing the percentages from each category
   
     total_expenses = 0
     strings = []
   
     for category in categories:
         total_expenses += category.withdraws
-        strings.append(category.name)
-      
+        strings.append(category.name)       # Create a list with the names of each category
+
+
     pct_category = []
 
     for category in categories:
         pct_category.append(round(category.withdraws/total_expenses * 100, 0))
       
-
+    # print(pct_category)
     rounded = []
     for pct in pct_category:
-        if pct % 10 > 5:
+        if pct < 10:
+            rounded.append(0)
+        elif (pct % 10) / 10 > 0.5:
             rounded.append((pct // 10 + 1) * 10 )
         else:
             rounded.append((pct // 10) * 10 )
 
       
     # print(rounded)
-  
+
+    
+    # Starts the string
     title = "Percentage spent by category\n"
 
     percentages = ["100| ", " 90| ", " 80| ", " 70| ", " 60| ", 
@@ -91,6 +96,9 @@ def create_spend_chart(categories):
 
     expenses = ""
     counter = 110
+
+    ## Graphs the points ans spaces
+    
     for percentage in percentages:
       
         counter = counter - 10
@@ -102,31 +110,35 @@ def create_spend_chart(categories):
           else:
             expenses += "o  "
             
-        expenses += "\n" 
+        expenses += "\n"
 
+    # Graphs the lines for the report
     line = (" "* 4) + "-" + ("---"* len(rounded)) + "\n"
 
-  
+    # Graph the word below each percentage
+    ## Finds the longest word in the string list
     longest = 0
     for text in strings:
       if len(text) > longest:
         longest = len(text)
         
-
+    ## Normalize each word to get the same len to all the categogies
     for i in range(len(strings)):
       strings[i] = strings[i] + " " * (longest - len(strings[i]))
 
-    
+      
+    ## Prints each letter.
     bottom = ""
-
+    
     for i in range(len(strings[0])):
       bottom += "    "
       for text in strings:
         bottom += " " + text[i] + " "
-
-      bottom += "\n"
-
-
+    
+      bottom += " \n"
+      
+    bottom = bottom.rstrip() + "  "
+  
     return title + expenses + line + bottom
 
     
